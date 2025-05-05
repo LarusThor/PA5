dcb6ae1b15b7cf4415a6b6ca6b711cc2c544bdd5
LarusThor <larus23@ru.is>
2025-05-05 20:36:29 +0000

    update_added_clientinfo

--- main.cpp
+++ main.cpp
@@ -7,22 +7,39 @@ std::vector<char> vBuffer(20 * 1024);
 struct ClientSession {
     std::string username;
     asio::ip::tcp::socket socket;
+
+    ClientSession(asio::io_context& io_context) : socket(io_context){}
 };
+// [1]: Hello
 
 std::vector<std::shared_ptr<ClientSession>> clients;
 
 int main(){
     
+
+
     try{
         asio::io_context io_context;
-    
+        
+        // Create an acceptor to listen on TCP port 12345
         asio::ip::tcp::acceptor acceptor(io_context, 
-        asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 12345));
-    
+            asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 12345));
+        
+        //asio::error_code ec;
+        //asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 12345);
         std::cout << "Server is running at port 12345" << '\n';
 
-        asio::ip::tcp::socket socket(io_context);
+        asio::ip::tcp::socket socket(io_context); 
         acceptor.accept(socket);
+
+        ClientSession client(io_context);
+
+        std::string username;
+        std::cout << "Write your username: \n";
+        std::cin >> username;
+
+        client.socket = std::move(socket);
+        client.username = username;
 
         std::cout << "Client conntected \n";
 
@@ -31,8 +48,10 @@ int main(){
         while(true){
             char data[1024] = {0};
             asio::error_code error;
+            std::string message = "[ " + client.username + " ]:\n";
+            asio::write(client.socket, asio::buffer(message), error);
 
-            size_t length = socket.read_some(asio::buffer(data), error);
+            size_t length = client.socket.read_some(asio::buffer(data), error);
 
             if(error == asio::error::eof){
                 std::cout << "Client disconnected \n";
